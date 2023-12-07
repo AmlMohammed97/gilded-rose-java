1d396524643f723abca84609ce02cc18dc780a01
Aml Mohammed <am-cw@testaviva.dk>
2023-12-07 14:43:37 +0200

    enhancement: add unit tests for GildedRose

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,11 +7,93 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class GildedRoseTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    void testUpdateQualityIfItemIsSulfuras() {
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 10, 10) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals(10, app.items[0].sellIn);
+        assertEquals(10, app.items[0].quality);
+    }
+
+    @Test
+    void testUpdateQualityIfItemIsAgedBrieAfterTheConcert() {
+        Item[] items = new Item[] { new Item("Aged Brie", 0, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(-1, app.items[0].sellIn);
+        assertEquals(0, app.items[0].quality);
+    }
+
+    @Test
+    void testUpdateQualityIfItemIsAgedBrieIfTenDaysOrLessRemaining() {
+        Item[] items = new Item[] { new Item("Aged Brie", 9, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(8, app.items[0].sellIn);
+        assertEquals(12, app.items[0].quality);
+    }
+ 
+    @Test
+    void testUpdateQualityIfItemIsAgedBrieIfFiveDaysOrLessRemaining() {
+        Item[] items = new Item[] { new Item("Aged Brie", 5, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(4, app.items[0].sellIn);
+        assertEquals(13, app.items[0].quality);
+    }
+
+    @Test
+    void testUpdateQualityIfItemIsBackstageAfterTheConcert() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 0, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(-1, app.items[0].sellIn);
+        assertEquals(0, app.items[0].quality);
+    }
+
+    @Test
+    void testUpdateQualityIfItemIsBackstageIfTenDaysOrLessRemaining() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 10, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(9, app.items[0].sellIn);
+        assertEquals(12, app.items[0].quality);
+    }
+ 
+    @Test
+    void testUpdateQualityIfItemIsBackstageIfFiveDaysOrLessRemaining() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 3, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(2, app.items[0].sellIn);
+        assertEquals(13, app.items[0].quality);
+    }
+
+    @Test
+    void testUpdateQualityIfItemQualityIsFifty() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 3, 50) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(2, app.items[0].sellIn);
+        assertEquals(50, app.items[0].quality);
+    }
+
+    @Test
+    void testUpdateQualityForNormalItemIfItemQualityIsZero() {
+        Item[] items = new Item[] { new Item("Normal item", 3, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(2, app.items[0].sellIn);
+        assertEquals(0, app.items[0].quality);
+    }
+
+    @Test
+    void testUpdateQualityForNormalItemIfItemSellInDateIsZero() {
+        Item[] items = new Item[] { new Item("Normal item", 0, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(-1, app.items[0].sellIn);
+        assertEquals(8, app.items[0].quality);
     }
 
 }
